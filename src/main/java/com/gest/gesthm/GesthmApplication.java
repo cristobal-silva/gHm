cce16b1665acf6ac7c6eb60745e0fcae5507b1fb
cristobal-silva <cristobalsilvamu@gmail.com>
2024-11-22 00:08:33 -0300

    primer commit

---
+++ src/main/java/com/gest/gesthm/GesthmApplication.java
@@ -0,0 +1,13 @@
+package com.gest.gesthm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesthmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesthmApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/gest/gesthm/entity/Paciente.java
@@ -0,0 +1,82 @@
+package com.gest.gesthm.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "pacientes")
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String telefono;
+    private String email;
+    @Column(name = "fecha_de_nacimiento")
+    @Temporal(TemporalType.DATE)
+    private Date fechaDeNacimiento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    the
+
+    void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    the String
+
+    getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    the String
+
+    getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(Date fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+}
